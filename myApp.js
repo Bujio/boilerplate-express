9a4ef3cebb134eedb6aad7d155559f8d8c5b2e84
Bujio <javier.vivas@icloud.com>
2023-09-16 17:57:09 +0200

    add modify myapp

--- myApp.js
+++ myApp.js
@@ -8,8 +8,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const mySecret = process.env["MESSAGE_STYLE"];
+
 app.get("/json", (req, res) => {
-  const mySecret = process.env["MESSAGE_STYLE"];
   if (process.env.MESSAGE_STYLE === "uppercase") {
     res.json({
       message: "Hello json".toUpperCase(),
