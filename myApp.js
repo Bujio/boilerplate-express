51d4acda32d242445416aa4d623aaed9f89ba698
Bujio <javier.vivas@icloud.com>
2023-09-16 16:51:06 +0200

    add file html app.get

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require("express");
 console.log("Hello World");
 
 let app = express();
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
