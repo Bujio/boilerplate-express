4b663edb8e06882758d7edba1ed0cb0e10f67e2e
Bujio <javier.vivas@icloud.com>
2023-09-16 17:01:32 +0200

    add middleware app.use

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 let express = require("express");
 console.log("Hello World");
-
 let app = express();
+
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
