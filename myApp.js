13349e145a0f9407e0dfc20adb15bd9ee42eea5c
Bujio <javier.vivas@icloud.com>
2023-09-16 17:09:39 +0200

    add app.get json

--- myApp.js
+++ myApp.js
@@ -7,4 +7,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => res.json({ message: "Hello json" }));
+
 module.exports = app;
